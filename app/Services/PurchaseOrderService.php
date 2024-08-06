89f874fb13be03f1fe6d10eb5085cd3bf47ed2cf
Anson Joseph <ansonpjp@gmail.com>
2024-08-06 19:09:00 +1200

    refactored the calculateTotal method into smaller methods

--- app/Services/PurchaseOrderService.php
+++ app/Services/PurchaseOrderService.php
@@ -11,56 +11,106 @@ use Nette\InvalidArgumentException;
  */
 class PurchaseOrderService extends BaseWebService
 {
+    /*
+     *  function to fetch the product details and calculate the total grouped by product_type_id
+     */
     public function calculateTotal(array $data)
     {
-        $promises = [];
-        foreach ($data['purchase_order_ids'] as $purchaseOrderId) {
-            $promises[] = $this->makeAsyncRequest('/CartonCloud_Demo/PurchaseOrders/' . $purchaseOrderId . '?version=5&associated=true');
-        }
-        // Wait to complete all REST CALLS
+        //  async call
+        $promises = $this->createAsyncRequests($data['purchase_order_ids']);
+
+        // handle the responses
         $responses = $this->handleAsyncResponse($promises);
 
-        $totals = [
-            1 => 0.0,
-            2 => 0.0,
-            3 => 0.0
-        ];
+        //process the responses
+        $result = $this->processResponse($responses, $data['purchase_order_ids']);
 
+        //prepare the results
+        return $this->prepareResult($result);
+
+    }
+
+    /*
+     * call the ASYNC call for all the purchaseOrderIds
+     */
+    public function createAsyncRequests(array $purchaseOrderIds)
+    {
+        return array_map(function ($purchaseOrderId) {
+           return $this->makeAsyncRequest('/CartonCloud_Demo/PurchaseOrders/' . $purchaseOrderId . '?version=5&associated=true');
+        }, $purchaseOrderIds);
+    }
+
+    /*
+     * Process the response to get the Totals and Failed Requests
+     */
+    public function processResponse($responses, array $purchaseOrderIds)
+    {
+        $totals = [
+          1 => 0.0,
+          2 => 0.0,
+          3 => 0.0
+        ];
         $failedRequests = [];
+
         foreach ($responses as $key => $response) {
             if($response && $response->successful()){
-                $json = $response->json();
-                $products = $json['data']['PurchaseOrderProduct'];
-
-                foreach ($products as $product) {
-                    try {
-                        $calculator = CalculatorFactory::getCalculator($product['product_type_id']);
-                        if($calculator){
-                            $totals[$product['product_type_id']] += $calculator->calculate($product);
-                        }
-                    } catch (InvalidArgumentException $e){
-                        Log::channel('slack')->error('Error in product calculation: ' . $e->getMessage());
-                    }
-                }
-            }
-            else{
-                $failedRequests[] = $data['purchase_order_ids'][$key];
-                $json = $response ? $response->json() : null;
-                Log::channel('slack')->error('Failed to fetch purchase order: ' . ($response ? $response->status() : 'No response') . '. Error message: ' . json_encode($json['info'] ?? '') . ' (for Purchase Order ID: ' . $data['purchase_order_ids'][$key] . ')');
+                $this->processSuccess($response, $totals);
+            } else{
+                $failedRequests[] = $purchaseOrderIds[$key];
+                $this->handleFailure($response, $purchaseOrderIds[$key]);
             }
         }
+        return [
+            'totals' => $totals,
+            'failedRequests' => $failedRequests
+        ];
+    }
 
-        $result = [];
-        foreach ($totals as $productTypeId => $total) {
+    /*
+     * Process the success by calling the Calculators
+     */
+    public function processSuccess($response, array &$totals)
+    {
+        $json = $response->json();
+        $products = $json['data']['PurchaseOrderProduct'];
+
+        foreach ($products as $product) {
+            try {
+                $calculator = CalculatorFactory::getCalculator($product['product_type_id']);
+                if($calculator){
+                    $totals[$product['product_type_id']] += $calculator->calculate($product);
+                }
+            }catch(InvalidArgumentException $e){
+                Log::channel('slack')->error('Error in product calculation: ' . $e->getMessage());
+            }
+        }
+    }
+
+    /*
+     * Log the Failures to Slack
+     */
+    public function handleFailure($response, $purchaseOrderId)
+    {
+        $json = $response ? $response->json() : null;
+        Log::channel('slack')->error('Failed to fetch purchase order: ' . ($response ? $response->status() : 'No response. ') .' Error message: ' . json_encode($json['info'] ?? '') . '( for purchase order ID:  )' . $purchaseOrderId . ')');
+    }
+
+    /*
+     * Prepare the result as an array containing results and failedrequests
+     */
+    public function prepareResult(array $data)
+    {
+        $result =[];
+        foreach ($data['totals'] as $productTypeId => $total) {
             $result[] = [
-                'product_type_id' => $productTypeId,
-                'total' => number_format((float)$total,1)
+              'product_type_id' => $productTypeId,
+              'total' => number_format((float)$total,1)
             ];
         }
 
         return [
-          'result' => $result,
-          'failedRequests' => $failedRequests
+            'result' => $result,
+            'failedRequests' => $data['failedRequests']
         ];
     }
 }
