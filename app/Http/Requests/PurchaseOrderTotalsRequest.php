72200ed33bf007e52162cebc42d587f3b335c87f
Anson Joseph <ansonpjp@gmail.com>
2024-08-06 20:08:06 +1200

    validations added to respond to the customer if the input is not correct

--- app/Http/Requests/PurchaseOrderTotalsRequest.php
+++ app/Http/Requests/PurchaseOrderTotalsRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class PurchaseOrderTotalsRequest extends FormRequest
 {
@@ -24,7 +26,31 @@ class PurchaseOrderTotalsRequest extends FormRequest
     public function rules()
     {
         return [
-            'purchase_order_ids' => 'required|array'
+            'purchase_order_ids' => 'required|array',
+            'purchase_order_ids.*' => 'integer',
         ];
+    }
+
+    /*
+     * Validation messages to the customer
+     */
+    public function messages()
+    {
+        return [
+            'purchase_order_ids.required' => 'The purchase order Id field is required',
+            'purchase_order_ids.array' => 'The purchase order Ids must be array',
+            'purchase_order_ids.*' => 'Each purchase order Id should be integer'
+        ];
+    }
+
+    /*
+    * failed validations to respond to the customer
+    */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation failed',
+            'errors' => $validator->errors()
+        ], 422));
     }
 }
